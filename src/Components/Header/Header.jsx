c0a91884a4c3f79d19903360841fdd50bf91e690
zuannn <zuanrachmawan234@gmail.com>
2024-09-25 15:28:55 +0700

    header-img

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,9 @@ import { assets } from "../../assets/assets";
 const Header = () => {
   return (
     <div className='header'>
+      <div className="header-img">
       <img src={assets.header_img} alt="" />
+      </div>
       <div className="header-content">
         <h2>order your favourite food here </h2>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem pariatur dolor consequuntur vero voluptate maxime culpa a molestias. Ab, totam.
